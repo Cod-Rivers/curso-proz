54282fd806c9ce9331d54cb274e13219bdfc9f5d
riverkirasamua <riversonsmorais@hotmail.com>
2023-05-02 02:15:45 -0300

    adicionado css

---
+++ aula javascript/permissao_usuario.js
@@ -0,0 +1,16 @@
+/*const nome = 'lampada'
+let sensorPresença = true
+let hora = 4
+
+if (sensorPresença = true && (hora <= 5 || hora >= 22)) {
+    console.log("luz acesa!!")
+}
+
+else {
+    console.log('luz apagada')
+}*/
+let senhaUsuario = document.createElement("senhaUsuario");
+let permissaoAdm
+
+console.log("Digite sua senha de 8 digitos")
+senhaUsuario
